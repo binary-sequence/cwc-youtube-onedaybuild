21935495500c3968d281fb9418a44a4919181234
Sergio Lindo <sergiolindo.empresa@gmail.com>
2020-06-29 17:58:10 +0200

    Initial Xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Sergio Lindo on 29/06/2020.
+//  Copyright © 2020 Software for life. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
