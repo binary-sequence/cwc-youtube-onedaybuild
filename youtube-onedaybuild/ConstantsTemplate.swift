77fce6b8924a70da6f9190372d63b1f5997477df
Sergio Lindo <sergiolindo.empresa@gmail.com>
2020-06-30 18:53:32 +0200

    Lesson 3.1 and 3.2

---
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  youtube-onedaybuild
+//
+//  Created by Sergio Lindo on 30/06/2020.
+//  Copyright © 2020 Software for life. All rights reserved.
+//
+
+import Foundation
+
+/*
+struct Constants {
+  static var API_KEY = ""
+  static var PLAYLIST_ID = ""
+  static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+*/
+
---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,17 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Sergio Lindo on 30/06/2020.
+//  Copyright © 2020 Software for life. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+  var videoId = ""
+  var title = ""
+  var description = ""
+  var thumbnail = ""
+  var published = ""
+}
